f31254b38460ca1f6bb1428382b1a3840fcb6175
Nithin Bekal <nithinbekal@gmail.com>
2024-01-18 00:31:04 -0500

    Move Value to its own file

--- lib/micrograd.rb
+++ lib/micrograd.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "micrograd/version"
+require_relative "micrograd/value"
 
 module Micrograd
   class NullOp
@@ -24,62 +25,6 @@ module Micrograd
   class PowOp < Struct.new(:x, :n)
     def backward(value)
       x.grad += n * (x ** (n-1)) * value.grad
-    end
-  end
-
-  class Value
-    def initialize(data, op: NullOp.new)
-      @data = data
-      @grad = 0.0
-      @op = op
-    end
-
-    attr_reader :data
-    attr_accessor :grad
-
-    def +(other)
-      other = wrap(other)
-      op = AddOp.new(self, other)
-
-      Value.new(@data + other.data, op: op)
-    end
-
-    def -(other) = self + (-wrap(other))
-
-    def *(other)
-      other = wrap(other)
-      op = MulOp.new(self, other)
-
-      Value.new(@data * other.data, op: op)
-    end
-
-    def /(other) = self * (wrap(other) ** -1)
-
-    def **(n)
-      op = PowOp.new(self, n)
-      Value.new(@data**n, op: op)
-    end
-
-    def -@ = Value.new(-self.data)
-
-    def backward
-      op.backward(self)
-    end
-
-    def coerce(other)
-      [Value.new(other), self]
-    end
-
-    def inspect
-      "Value(#{data}, grad: #{grad})"
-    end
-
-    private
-
-    attr_reader :op
-
-    def wrap(v)
-      v.is_a?(Value) ? v : Value.new(v)
     end
   end
 end
---
+++ lib/micrograd/value.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+module Micrograd
+  class Value
+    def initialize(data, op: NullOp.new)
+      @data = data
+      @grad = 0.0
+      @op = op
+    end
+
+    attr_reader :data
+    attr_accessor :grad
+
+    def +(other)
+      other = wrap(other)
+      op = AddOp.new(self, other)
+
+      Value.new(@data + other.data, op: op)
+    end
+
+    def *(other)
+      other = wrap(other)
+      op = MulOp.new(self, other)
+
+      Value.new(@data * other.data, op: op)
+    end
+
+    def **(n)
+      op = PowOp.new(self, n)
+      Value.new(@data**n, op: op)
+    end
+
+    def -@ = Value.new(-self.data)
+
+    def -(other) = self + (-wrap(other))
+    def /(other) = self * (wrap(other) ** -1)
+
+    def backward
+      op.backward(self)
+    end
+
+    def coerce(other)
+      [Value.new(other), self]
+    end
+
+    def inspect = "Value(#{data}, grad: #{grad})"
+
+    private
+
+    attr_reader :op
+
+    def wrap(v)
+      v.is_a?(Value) ? v : Value.new(v)
+    end
+  end
+end
